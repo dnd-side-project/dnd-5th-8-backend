105904733bccf85cbb6b40d3d92c30518acb8a67
cocomongg <pjm4142@naver.com>
2021-07-25 15:40:01 +0900

    Setting project

---
+++ eight/src/main/java/com/dnd/eight/EightApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.eight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EightApplication.class, args);
+	}
+
+}
---
+++ eight/src/test/java/com/dnd/eight/EightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.eight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
