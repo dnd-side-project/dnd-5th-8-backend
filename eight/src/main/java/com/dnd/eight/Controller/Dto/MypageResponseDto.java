4d9db451496309d91b336935c047052f9efbb2bc
soosungp33 <soosungp33@gmail.com>
2021-08-08 17:49:01 +0900

    feat : make mypage api api : /mypage/ + userId GET response : {   userName : String   userProfile : String   code : String } close ISSUE#9

---
+++ eight/src/main/java/com/dnd/eight/Controller/Dto/MypageResponseDto.java
@@ -0,0 +1,14 @@
+package com.dnd.eight.Controller.Dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class MypageResponseDto {
+    String userName;
+    String userProfile;
+    String code;
+}
