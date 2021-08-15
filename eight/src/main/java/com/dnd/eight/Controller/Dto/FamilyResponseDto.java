501826f7863257cacbae319ae933431091ad1a11
soosungp33 <soosungp33@gmail.com>
2021-08-15 22:09:04 +0900

    chore: modify RecieveLetterList add profile to familyResponseDto add userId to recieveLetterResponseDto

--- eight/src/main/java/com/dnd/eight/Controller/Dto/FamilyResponseDto.java
+++ eight/src/main/java/com/dnd/eight/Controller/Dto/FamilyResponseDto.java
@@ -8,4 +8,5 @@ import lombok.NoArgsConstructor;
 public class FamilyResponseDto {
     Long userId;
     String nickname;
+    String profile;
 }
--- eight/src/main/java/com/dnd/eight/Controller/Dto/RecieveLetterResponseDto.java
+++ eight/src/main/java/com/dnd/eight/Controller/Dto/RecieveLetterResponseDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class RecieveLetterResponseDto {
+    long userId;
     String recieveNickname;
     String content;
 }
